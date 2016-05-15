e5368b3a5aa04159d214a9cd4bdc2eb063012216
frankiec <frcrescioni@gmail.com>
2016-05-15 23:15:36 +0000

    second commit, routes work

--- api.js
+++ api.js
@@ -2,14 +2,14 @@ var express = require("express");
 var app = express();
 
 
-// configure Express app
+//configure express
 app.set('port', process.env.PORT || 3000);
 app.use(express.static('public'));
 app.use(require("body-parser").urlencoded({extended: true}));
 app.use('/api', require('cors')());
 app.use(express.static(__dirname + '../views'));
 
-// set template engine
+//set template engine
 var handlebars = require('express-handlebars').create({defaultLayout: 'main', extname: '.hbs'  });
 app.engine('hbs', handlebars.engine);
 app.set('view engine', 'hbs' );
@@ -18,6 +18,8 @@ app.set('view engine', 'hbs' );
 //routes to help remove the clutter
 var routes = require("./routes/routes.js")(app);
 
+
+//error messages
 app.use(function(req,res) {
     res.type('text/plain'); 
     res.status(404);
@@ -29,6 +31,7 @@ app.use(function(req,res){
     res.status(500).send({ error: '500 Internal Service Error'});
 });
 
+//helps confirm api loaded correctly 
 app.listen(app.get('port'), function() {
     console.log('Express started');    
 });
